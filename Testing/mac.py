071bcb3df79153942d21d855230010ade1f9d71a
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-05-21 14:03:33 +0800

    Update mac.py

--- Testing/mac.py
+++ Testing/mac.py
@@ -1,4 +1,4 @@
-from strategy import MovingAverageCrossStrategy
+from strategy import Strategy, MovingAverageCrossStrategy
 from event import SignalEvent
 from backtest import Backtest
 from data import SQLDataHandler
@@ -14,5 +14,6 @@ if __name__ == "__main__":
     heartbeat = 0.0
     start_date = dt(2010, 2,20, 0, 0, 0)
     end_date = dt.now()
-    backtest = Backtest(csv_dir, symbol_list, initial_capital, heartbeat, start_date, end_date, SQLDataHandler, SimulatedExecutionHandler, Portfolio, MovingAverageCrossStrategy)
+    backtest = Backtest(csv_dir=csv_dir, symbol_list=symbol_list, initial_capital=initial_capital, heartbeat=heartbeat, startdate=start_date, enddate=end_date, data_handler=HistoricCSVDataHandler, execution_handler=SimulatedExecutionHandler, portfolio=Portfolio, strategy=MovingAverageCrossStrategy, window=[30,90])
     backtest.simulate_trading(frequency=252)
+    

5d2abf5449b4f48443e767568d600e82f5d97778
Sariel Hovey <45866201+SarielHovey@users.noreply.github.com>
2020-10-19 13:33:51 +0800

    Update DBinitialization.sql
    Modify create commend for SQLite

--- Testing/DBinitialization.sql
+++ Testing/DBinitialization.sql
@@ -59,49 +59,49 @@ CREATE TABLE daily_price ( id int NOT NULL AUTO_INCREMENT,
 # if use sqlite, then use the below instead
                                                                                                             
 CREATE TABLE exchange ( 
-    id INT PRIMARY KEY, 
-    abbrev VARCHAR(32) NOT NULL, 
-    name VARCHAR(255) NOT NULL, 
-    city VARCHAR(255), 
-    country VARCHAR(255), 
-    currency VARCHAR(64), 
-    timezone_offset DATETIME, 
-    created_date DATETIME NOT NULL, 
-    last_updated_date DATETIME NOT NULL
-    );
+        id INTEGER PRIMARY KEY autoincrement, 
+        abbrev VARCHAR(32) NOT NULL, 
+        name VARCHAR(255) NOT NULL, 
+        city VARCHAR(255), 
+        country VARCHAR(255), 
+        currency VARCHAR(64), 
+        timezone_offset DATETIME, 
+        created_date DATETIME NOT NULL, 
+        last_updated_date DATETIME NOT NULL
+        );
 
 CREATE TABLE data_vendor ( 
-    id INT PRIMARY KEY, 
-    name varchar(64) NOT NULL, 
-    website_url varchar(255), 
-    support_email varchar(255), 
-    created_date datetime, 
-    last_updated_date datetime
-    );
+        id INTEGER PRIMARY KEY autoincrement, 
+        name varchar(64) NOT NULL, 
+        website_url varchar(255), 
+        support_email varchar(255), 
+        created_date datetime, 
+        last_updated_date datetime
+        );
     
 CREATE TABLE symbol ( 
-    id int PRIMARY KEY, 
-    exchange_id int KEY, 
-    ticker varchar(32) NOT NULL, 
-    instrument varchar(64) NOT NULL, 
-    name varchar(255), 
-    sector varchar(255), 
-    currency varchar(32), 
-    created_date datetime NOT NULL, 
-    last_updated_date datetime NOT NULL
-    );
+        id INTEGER PRIMARY KEY autoincrement, 
+        exchange_id int KEY, 
+        ticker varchar(32) NOT NULL, 
+        instrument varchar(64) NOT NULL, 
+        name varchar(255), 
+        sector varchar(255), 
+        currency varchar(32), 
+        created_date datetime NOT NULL, 
+        last_updated_date datetime NOT NULL
+        );
     
 CREATE TABLE daily_price ( 
-    id int PRIMARY KEY, 
-    data_vendor_id int KEY NOT NULL, 
-    symbol_id int KEY NOT NULL, 
-    price_date datetime NOT NULL, 
-    created_date datetime NOT NULL, 
-    last_updated_date datetime NOT NULL, 
-    open_price decimal(19,4), 
-    high_price decimal(19,4), 
-    low_price decimal(19,4), 
-    close_price decimal(19,4), 
-    adj_factor decimal(19,10), 
-    volume bigint NULL 
-    );
+        id INTEGER PRIMARY KEY autoincrement, 
+        data_vendor_id int KEY NOT NULL, 
+        symbol_id int KEY NOT NULL, 
+        price_date datetime NOT NULL, 
+        created_date datetime NOT NULL, 
+        last_updated_date datetime NOT NULL, 
+        open_price decimal(19,4), 
+        high_price decimal(19,4), 
+        low_price decimal(19,4), 
+        close_price decimal(19,4), 
+        adj_factor decimal(19,10), 
+        volume bigint
+        );
